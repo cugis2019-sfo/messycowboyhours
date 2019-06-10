86a596f0e51125d439165c0c9d42aff1eb1e0719
lilythecowboy <51681125+lilythecowboy@users.noreply.github.com>
2019-06-10 16:51:27 -0700

    Add files via upload

---
+++ messycowboyhours.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+5/8 #division
+print("I'm just a sad cowboy")
+
+print("they always say yeehaw but never hawyee")
+23*3 #multiplication
+print("nice")
+4*15
+78-2400
+5^2
+5+2
+400+20
+5**2
+print("i may or may not be inappropriate rn")
+print("how do you delete results")
+print("hi 5")
+def sum(a,b):
+    sum = a + b
+    
+    print("the sum of", a, "and", b, "is", sum)
+
+sum(2,6)    
+print("i cant do this")
+print("i freaking did it!!!")     
+def sum(f,r,m):
+    sum = f + r + m
+    print("the sum of", f, r, "and", m, "is", sum)
+    
+sum(17, 12, 40)
+print("nice")
+print(you're so close")
+print("you only have four more until you ascend to peak level")
+sum(33,33,0)
+print("two more")
+print("nice nice nice nice nice nice nice nice")
+
+def trianglearea(b,h):
+    trianglearea = 0.5*b*h
+    print("the area of this triangle is", trianglearea)
+    
+trianglearea(2,10)
